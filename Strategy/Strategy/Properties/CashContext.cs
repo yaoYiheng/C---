1ccefaf230b784541c4774b8e5016e7b37746182
yaoYiheng <fn1117@qq.com>
2019-01-08 00:35:20 +0800

    策略与简单工厂结合
    持有折扣方式的基类引用
    在构造CashContext时, 通过传入的字符串, 生成对应的折扣类型的类对象. 然后统一调用计算.

--- Strategy/Strategy/Properties/CashContext.cs
+++ Strategy/Strategy/Properties/CashContext.cs
@@ -3,10 +3,34 @@ namespace Strategy.Properties
 {
     public class CashContext
     {
-        private BaseCash m_BaseCash;
-        public CashContext(BaseCash baseCash)
+        private BaseCash m_BaseCash = null;
+        //public CashContext(BaseCash baseCash)
+        //{
+        //    this.m_BaseCash = baseCash;
+        //}
+
+        //public double Calculate(double amount)
+        //{
+        //    return m_BaseCash.TakeCash(amount);
+        //}
+
+
+        public CashContext (string type)
         {
-            this.m_BaseCash = baseCash;
+            switch(type)
+            {
+                case "正常":
+                    m_BaseCash = new NormalCash();
+                    break;
+                case "打8折":
+                    m_BaseCash = new DiscountCash("0.8");
+                    break;
+                case "满300, 返100":
+                    m_BaseCash = new ReturnCash("300", "100");
+                    break;
+                default:
+                    break;
+            }
         }
 
         public double Calculate(double amount)
